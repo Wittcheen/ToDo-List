ababbb67f7ff305edfe867d89623cdc9e9821862
Wittcheen <wittchenchris@gmail.com>
2023-03-21 10:18:42 +0100

    Created wpf project and added usercontrols feature

---
+++ ToDo-List/Client/UserControls/ContentArea.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ContentArea.xaml
+    /// </summary>
+    public partial class ContentArea : UserControl
+    {
+        private static ContentArea? Instance;
+
+        public ContentArea()
+        {
+            InitializeComponent();
+            Instance ??= this;
+        }
+
+        private static void Navigator(UserControl control) => Instance.Content = control;
+
+        public static void NavigateToSignIn() => Navigator(new SignIn());
+    }
+}
